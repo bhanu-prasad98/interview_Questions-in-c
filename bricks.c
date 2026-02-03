fe978acfb005b546a014299ff20d4e312d7075e1
bhanu-prasad98 <seguribhanuprasad98@gmail.com>
2026-02-03 15:56:21 +0530

    Add files via upload

---
+++ bricks.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+     void main()
+  {
+    int big;
+    int small;
+    int goal;
+    printf("enter the values of the big small and goal\n");
+    scanf("%d %d %d",&big,&small,&goal);
+    int required=goal/5;
+    if(required<=big)
+ {
+    int rem=goal-(required*5);
+    if(rem<=small)
+{
+    printf("true");
+}
+    else
+{
+    int rem=goal-(big*5);
+    if(rem<=small)
+{
+    printf("false");
+}
+    else
+{
+    printf("false");
+}
+ }
+  }
+   } 
